853db4e59ebb1adc56f9b46c8a7a5805bbcb9a8a
Matheus Marin <marinnmatheus@gmail.com>
2020-12-28 20:48:04 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.3.0.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
+    application
+ }
+
+group = "com.kotlinspring"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+
+    testImplementation("io.kotest:kotest-runner-junit5:4.3.0")
+    testImplementation("io.kotest:kotest-extensions-spring:4.3.0")
+
+    testImplementation("io.mockk:mockk:1.10.0")
+
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.4.0")
+
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "com.kotlinspring.ApplicationKt"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
