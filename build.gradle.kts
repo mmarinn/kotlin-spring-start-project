9accae9faba9225da2e5054b2f4a6087812951ca
Matheus Marin <marinnmatheus@gmail.com>
2021-02-25 21:18:38 -0300

    adjust dependencies order on gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,27 +22,22 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
     implementation("org.springframework.boot:spring-boot-starter-web")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.postgresql:postgresql")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.4.0")
 
     testImplementation("io.kotest:kotest-runner-junit5:4.3.0")
     testImplementation("io.kotest:kotest-extensions-spring:4.3.0")
     testImplementation("io.kotest:kotest-assertions-json:4.4.1")
-
     testImplementation("io.mockk:mockk:1.10.0")
-
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:junit-jupiter:1.15.2")
-
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.4.0")
     testImplementation("org.testcontainers:postgresql:1.15.2")
-
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 dockerCompose {
